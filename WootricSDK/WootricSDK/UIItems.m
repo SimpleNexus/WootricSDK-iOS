f6e8057d7e62228aa8b2f15f08fbc7c8bc14cea6
Jason Rodriguez <jrodriguez@simplenexus.com>
2021-03-29 02:55:47 -0700

    Adds font bundle

--- WootricSDK/WootricSDK/UIItems.m
+++ WootricSDK/WootricSDK/UIItems.m
@@ -30,7 +30,12 @@
 @implementation UIItems
 
 + (void)dynamicallyAddFont {
+  public let fontBundle = Bundle.module
   NSString *fontPath = [[NSBundle bundleForClass:[UIItems class]] pathForResource:@"fontawesome-webfont" ofType:@"ttf"];
+  if ([fontPath length] == 0) {
+    // Try to find the font in the SPM bundle
+    fontPath = [SWIFTPM_MODULE_BUNDLE pathForResource:@"fontawesome-webfont" ofType:@"ttf"];
+  }
   
   CFErrorRef error;
   NSURL *url = [NSURL fileURLWithPath:fontPath isDirectory:NO];
