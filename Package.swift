fe60a47cbe0af70290fa0788b4af2c926ee2d3ea
Jason Rodriguez <jrodriguez@simplenexus.com>
2021-03-29 02:36:29 -0700

    Fixes package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,17 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "WootricSDK",
-    // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "WootricSDK", targets: ["WootricSDK"])
     ],
-    dependencies: [],
     targets: [
         .target(
             name: "WootricSDK",
-            dependencies: [],
-            path: "WootricSDK/WootricSDK"
+            path: "WootricSDK/WootricSDK",
+            resources: [.process("fontawesome-webfont.tff"), .process("info.plist")],
+            publicHeadersPath: "."
         )
     ]
 )
